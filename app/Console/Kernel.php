f92d4e2100b7d335a6aebbb34813aa2e8bb20396
stefutz101 <60782567+stefutz101@users.noreply.github.com>
2021-02-14 13:21:20 +0100

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:cronjob')->everyMinute();
+        $schedule->command('command:cronjob')->everyFifteenMinutes();
     }
 
     /**
